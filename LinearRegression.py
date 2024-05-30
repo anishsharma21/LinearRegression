c3aa6dec84832aa0945c619b56bbde87ce4e2c3f
anishsharma21 <anisharma2103@gmail.com>
2024-05-30 22:02:35 +1000

    - Setting up the LinearRegression class with some simple methods (just initialising them for the time being) - Instantiating the LinearRegression class in the notebook and using its predict_y values function

--- LinearRegression.py
+++ LinearRegression.py
@@ -2,6 +2,24 @@ import numpy as np
 import math
 
 class LinearRegression:
-    m = 0
-    b = 0
+    def __init__(self) -> None:
+        self.m = 1
+        self.b = 0
+    
+    def y_predict(self, x):
+        return self.m * x + self.b
+    
+    def standard_fit(x, y):
+        # diff between y and y_predict
+        print(x, y)
+    
+    def experimental_fit(x, y):
+        # diff between y_predict and line (shortest distance)
+        print(x, y)
+    
+    def loss_standard(y, y_predicted):
+        print(y, y_predicted)
+    
+    def loss_experimental(y, y_predicted):
+        print(y, y_predicted)
 
