4b78515da43b13cc771817f95f05f208571a843c
anishsharma21 <anisharma2103@gmail.com>
2024-06-03 23:59:25 +1000

    - Crude line of best fit method (but it doesn't work for negative downward graphs)

---
+++ ErrorDirection.py
@@ -0,0 +1,5 @@
+import enum
+
+class ErrorDirection(enum.Enum):
+    ERROR_INCREASED = 1
+    ERROR_DECREASED = -1
--- LinearRegression.py
+++ LinearRegression.py
@@ -1,27 +1,52 @@
 import pandas as pd
+from ErrorDirection import ErrorDirection
 
 class LinearRegression:
     def __init__(self) -> None:
         self.m = 1
         self.b = 0
         self.points = []
+        self.learning_rate = 0
+        self.iterations = 0
 
-    def crude_fit(self, learning_rate=0.01):
-        num_points = (len(self.points))
-        av_SSE = self.sum_of_squared_differences(self.m, self.b) / num_points
-        print(av_SSE)
-        prev_m = self.m
-        prev_b = self.b
-        self.m += learning_rate
-        self.b += learning_rate
-        new_m_av_SSE = self.sum_of_squared_differences(self.m, prev_b) / num_points
-        new_b_av_SSE = self.sum_of_squared_differences(prev_m, self.b) / num_points
-        print(new_b_av_SSE)
-        print(new_m_av_SSE)
-        b_direction = self.multiplier(av_SSE, new_b_av_SSE)
-        m_direction = self.multiplier(av_SSE, new_m_av_SSE)
-        print(b_direction, m_direction)
+    def crude_fit(self, learning_rate = 0.5):
+        self.learning_rate = learning_rate
+        initial_SSE = self.sum_of_squared_differences(self.m, self.b)
+        new_b = self.b + learning_rate
+        mod_b_SSE = self.sum_of_squared_differences(self.m, new_b)
+        b_error_direction = self.error_direction(initial_SSE, mod_b_SSE)
+        final_b = self.find_b_crude(initial_SSE, b_error_direction)
+        new_m = self.m + learning_rate
+        mod_m_SSE = self.sum_of_squared_differences(new_m, self.b)
+        m_error_direction = self.error_direction(initial_SSE, mod_m_SSE)
+        final_m = self.find_m_crude(initial_SSE, m_error_direction)
+        print(f"B-Value: {final_b}")
+        print(f"M-value: {final_m}")
+        print(f"Total iterations: {self.iterations}")
+
+    def find_b_crude(self, current_SSE: float, error_direction: ErrorDirection):
+        self.iterations += 1
+        new_b = self.b + self.learning_rate * (-error_direction.value)
+        mod_b_SSE = self.sum_of_squared_differences(self.m, new_b)
+        b_error_direction = self.error_direction(current_SSE, mod_b_SSE)
+        if b_error_direction == ErrorDirection.ERROR_INCREASED:
+            return self.b
+        else:
+            self.b = new_b
+            return self.find_b_crude(mod_b_SSE, error_direction)
     
+    def find_m_crude(self, current_SSE: float, error_direction: ErrorDirection):
+        self.iterations += 1
+        new_m = self.m + self.learning_rate * (-error_direction.value)
+        mod_m_SSE = self.sum_of_squared_differences(new_m, self.b)
+        m_error_direction = self.error_direction(current_SSE, mod_m_SSE)
+        if m_error_direction == ErrorDirection.ERROR_INCREASED:
+            return self.m
+        else:
+            self.m = new_m
+            return self.find_m_crude(mod_m_SSE, error_direction)
+
+
     def standard_fit(self):
         # minimising sum of square differences
         SSE = self.sum_of_squared_differences()
@@ -37,8 +62,8 @@ class LinearRegression:
     def error_squared(self, y, y_predicted):
         return (y - y_predicted) ** 2
 
-    def multiplier(self, original_SSE, new_SSE):
-        return -1 if original_SSE - new_SSE < 0 else 1
+    def error_direction(self, previous_SSE, new_SSE):
+        return ErrorDirection.ERROR_INCREASED if previous_SSE - new_SSE < 0 else ErrorDirection.ERROR_DECREASED
 
     def x_y_to_points(self, x_vals: pd.Series, y_vals: pd.Series):
         print(len(x_vals))
