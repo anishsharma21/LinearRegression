42f01a392827efe432281426097012d2636223d1
anishsharma21 <anisharma2103@gmail.com>
2024-06-04 17:12:29 +1000

    - Finally implemented the crude_fit function for linear regression and have both working in the LinearRegression class

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,78 +1,41 @@
 import pandas as pd
 from ErrorDirection import ErrorDirection
+import numpy as np
 
 class LinearRegression:
     def __init__(self) -> None:
         self.m = 1
         self.b = 0
         self.points = []
-        self.learning_rate = 0
-        self.iterations = 0
+        self.learning_rate = 0.0001
+        self.iterations = 1000
 
-    def crude_fit(self, learning_rate = 0.5): 
-        self.learning_rate = learning_rate
-        # first we get the initial SSE with our initialised m and b variables
-        initial_SSE = self.sum_of_squared_differences(self.m, self.b)
-        # lets add a little bit to the learning_rate
-        b_mod = self.b + learning_rate
-        # now lets calculate the SSE with the new b value
-        new_b_SSE = self.sum_of_squared_differences(self.m, b_mod)
-        # now lets calculate the difference between the initial and new SSE
-        error_direction = self.error_direction(initial_SSE, new_b_SSE)
-        # now, if the error decreased, then we should keep adding values until the error increases, and if the error increased, then we should minus value until the error increases
-        if error_direction == ErrorDirection.ERROR_DECREASED:
-            while error_direction != ErrorDirection.ERROR_INCREASED:
-                initial_SSE = new_b_SSE
-                self.b = b_mod
-                b_mod = self.b + learning_rate
-                new_b_SSE = self.sum_of_squared_differences(self.m, b_mod)
-                error_direction = self.error_direction(initial_SSE, new_b_SSE)
-        elif error_direction == ErrorDirection.ERROR_INCREASED:
-            b_mod = self.b - learning_rate
-            new_b_SSE = self.sum_of_squared_differences(self.m, b_mod)
-            error_direction = self.error_direction(initial_SSE, new_b_SSE)
-            while error_direction != ErrorDirection.ERROR_INCREASED:
-                initial_SSE = new_b_SSE
-                self.b = b_mod
-                b_mod = self.b - learning_rate
-                new_b_SSE = self.sum_of_squared_differences(self.m, b_mod)
-                error_direction = self.error_direction(initial_SSE, new_b_SSE)
-        print(self.b)
+    def crude_fit(self):
+        x = np.array([point[0] for point in self.points])
+        y = np.array([point[1] for point in self.points])
+        n = len(self.points)
+        print(n)
 
-        # first we get the initial SSE with our initialised m and b variables
-        initial_SSE = self.sum_of_squared_differences(self.m, self.b)
-        # lets add a little bit to the learning_rate
-        m_mod = self.m + learning_rate
-        # now lets calculate the SSE with the new b value
-        new_m_SSE = self.sum_of_squared_differences(m_mod, self.b)
-        # now lets calculate the difference between the initial and new SSE
-        error_direction = self.error_direction(initial_SSE, new_m_SSE)
-        # now, if the error decreased, then we should keep adding values until the error increases, and if the error increased, then we should minus value until the error increases
-        if error_direction == ErrorDirection.ERROR_DECREASED:
-            while error_direction != ErrorDirection.ERROR_INCREASED:
-                initial_SSE = new_m_SSE
-                self.m = m_mod
-                m_mod = self.m + learning_rate
-                new_m_SSE = self.sum_of_squared_differences(m_mod, self.b)
-                error_direction = self.error_direction(initial_SSE, new_m_SSE)
-        elif error_direction == ErrorDirection.ERROR_INCREASED:
-            m_mod = self.m - learning_rate
-            new_m_SSE = self.sum_of_squared_differences(m_mod, self.b)
-            error_direction = self.error_direction(initial_SSE, new_m_SSE)
-            while error_direction != ErrorDirection.ERROR_INCREASED:
-                initial_SSE = new_m_SSE
-                self.m = m_mod
-                m_mod = self.m - learning_rate
-                new_m_SSE = self.sum_of_squared_differences(m_mod, self.b)
-                error_direction = self.error_direction(initial_SSE, new_m_SSE)
-        print(self.m)
+        for _ in range(self.iterations):
+            y_pred = self.m * x + self.b
+            error = y_pred - y
+            m_gradient = (2 / n) * np.sum(error * x)
+            b_gradient = (2 / n) * np.sum(error)
+
+            self.m = self.m - self.learning_rate * m_gradient
+            self.b = self.b - self.learning_rate * b_gradient
+
+            new_error = np.sum((y - (self.m * x + self.b)) ** 2)
+
+            if new_error < 0.0001:
+                break
 
 
     def standard_fit(self):
         num_points = len(list(self.points))
-        sum_of_y = sum([y for x, y in self.points])
-        sum_of_x = sum([x for x, y in self.points])
-        sum_of_x_squared = sum([x**2 for x, y in self.points])
+        sum_of_y = sum([y for _, y in self.points])
+        sum_of_x = sum([x for x, _ in self.points])
+        sum_of_x_squared = sum([x**2 for x, _ in self.points])
         sum_of_product_of_x_and_y = sum([x*y for x, y in self.points])
 
         self.m = (num_points * sum_of_product_of_x_and_y - sum_of_x * sum_of_y) / (num_points * sum_of_x_squared - sum_of_x ** 2)
